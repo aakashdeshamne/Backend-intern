a42abe95de214d9b1faa2fc0872c290328468a7e
aakash deshmane <127671909+aakashdeshamne@users.noreply.github.com>
2024-02-02 20:08:17 +0530

    Add files via upload

---
+++ day12/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+    int p = 10;
+    protected int a = 10;
+
+    public void type() {
+        System.out.println("");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
